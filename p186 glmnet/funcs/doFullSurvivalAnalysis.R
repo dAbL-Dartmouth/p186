6b448941f70b2bb25002799943a70314b56304b4
dAbL-Dartmouth <57950452+dAbL-Dartmouth@users.noreply.github.com>
2019-11-27 12:34:42 -0500

    Update doFullSurvivalAnalysis.R

--- p186 glmnet/funcs/doFullSurvivalAnalysis.R
+++ p186 glmnet/funcs/doFullSurvivalAnalysis.R
@@ -1,9 +1,5 @@
 # ******************************************************************************
-# Modified by : Joshua Weiner for Co-immunization of DNA and Protein in the Same Anatomical Sites Induces Superior Protective Immune Responses against SHIV Challenge
-# Originally Produced by
-# Author      : Srivamshi Pittala
-# Advisor     : Prof. Chris Bailey-Kellogg
-# Project     : Profectus T2
+# # Prepared by Joshua Weiner for Co-immunization of DNA and Protein in the Same Anatomical Sites Induces Superior Protective Immune Responses against SHIV Challenge
 # Decription  : Performs survival analysis using the input features
 # ******************************************************************************
 
